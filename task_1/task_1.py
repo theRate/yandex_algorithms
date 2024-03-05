17b9a3718b42ecda6f7fed1163eba31e2d68e3a3
Alexander Mareyev <adskiypovar@gmail.com>
2024-03-05 17:22:36 +0300

    Create task_1.py

---
+++ task_1/task_1.py
@@ -0,0 +1,14 @@
+v_anchor, v_range = map(int, input().split())
+m_anchor, m_range = map(int, input().split())
+
+anchors = [v_anchor, m_anchor]
+anchors.sort()
+result = 0
+if anchors[1] - anchors[0] > v_range + m_range:
+    result = (v_range + m_range) * 2 + 2
+else:
+    min_range = min(v_anchor - v_range, m_anchor - m_range)
+    max_range = max(v_anchor + v_range, m_anchor + m_range)
+    result = len(range(min_range, max_range + 1))
+
+print(result)
