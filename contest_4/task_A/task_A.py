ac38609d65741fdc69c5605d7ed7c44e6039bf24
Alexander Mareyev <adskiypovar@gmail.com>
2024-03-28 21:23:08 +0300

    Create task_A.py

---
+++ contest_4/task_A/task_A.py
@@ -0,0 +1,37 @@
+n = int(input())
+n_list = list(map(int, input().split()))
+k = int(input())
+k_list = []
+for _ in range(k):
+    k_list.append(list(map(int, input().split())))
+
+n_list.sort()
+result = []
+
+for b in k_list:
+    l = 0
+    r = n - 1
+    while l < r:
+        m = (l + r) // 2
+        if n_list[m] >= b[0]:
+            r = m
+        else:
+            l = m + 1
+
+    left_index = l
+    r = n - 1
+    while l < r:
+        m = (l + r + 1) // 2
+        if n_list[m] <= b[1]:
+            l = m
+        else:
+            r = m - 1
+
+    right_index = l
+
+    if left_index == right_index and not b[0] <= n_list[left_index] <= b[1]:
+        result.append(0)
+    else:
+        result.append(len(n_list[left_index:right_index]) + 1)
+
+print(*result)
